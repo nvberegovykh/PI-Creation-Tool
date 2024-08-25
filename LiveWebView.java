f822ca2d0ac105fd728f56d93ddde1aa66925519
nvberegovykh <116811305+nvberegovykh@users.noreply.github.com>
2024-08-24 22:52:41 -0400

    Add files via upload

---
+++ LiveWebView.java
@@ -0,0 +1,25 @@
+package com.beregovykh.studio;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.webkit.WebView;
+
+public class LiveWebView extends WebView {
+
+    public LiveWebView(Context context) {
+        super(context);
+    }
+
+    public LiveWebView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public LiveWebView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    @Override
+    public boolean onCheckIsTextEditor() {
+        return true;
+    }
+}
---
+++ MainActivity.java
@@ -0,0 +1,390 @@
+package com.beregovykh.studio;
+
+
+import static android.view.View.SYSTEM_UI_FLAG_IMMERSIVE;
+import static android.view.View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION;
+import static android.view.View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR;
+
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.ActivityNotFoundException;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.Uri;
+import android.net.http.SslError;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+import android.webkit.SslErrorHandler;
+import android.webkit.ValueCallback;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.core.view.WindowInsetsControllerCompat;
+
+import java.io.File;
+import java.io.IOException;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final int REQUEST_CODE_LOLIPOP = 1;
+    private final static int RESULT_CODE_ICE_CREAM = 2;
+    private WebView webView;
+    private ValueCallback<Uri[]> mFilePathCallback;
+    private String mCameraPhotoPath;
+    private ValueCallback<Uri> mUploadMessage;
+
+
+    private final String mainURL = "https://dmv.ny.gov/contact-us/office-locations?address=null&radius=10&county=null&city=null";
+    private final String WebsiteName = "dmv.ny.gov";
+
+    @Override
+    public void onBackPressed() {
+        WebView webView = findViewById(R.id.webView);
+        if(webView!= null && webView.canGoBack())
+            webView.goBack();
+        else
+            super.onBackPressed();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            Window w = getWindow();
+            w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,  WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+            w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN);
+        }
+
+        WindowInsetsControllerCompat windowInsetsController =
+                ViewCompat.getWindowInsetsController(getWindow().getDecorView());
+        if (windowInsetsController == null) {
+            return;
+        }
+
+        windowInsetsController.setAppearanceLightNavigationBars(true);
+
+
+
+
+        WebView webView = findViewById(R.id.webView);
+        webView.getSettings().setDomStorageEnabled(true);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setLoadsImagesAutomatically(true);
+
+
+
+
+
+        ConnectivityManager connectivityManager = (ConnectivityManager)
+                getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+        if (networkInfo == null || !networkInfo.isConnected() || !networkInfo.isAvailable()) {
+            Dialog dialog = new Dialog(this);
+            dialog.setContentView(R.layout.alert_dialog);
+            dialog.setCanceledOnTouchOutside(false);
+            dialog.getWindow().setLayout(WindowManager.LayoutParams.MATCH_PARENT,
+                    WindowManager.LayoutParams.MATCH_PARENT);
+            dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.BLACK));
+            dialog.getWindow().getAttributes().windowAnimations =
+                    android.R.style.Animation_Toast;
+            Button btTryAgain = dialog.findViewById(R.id.bt_try_again);
+            btTryAgain.setOnClickListener(view -> recreate());
+            dialog.show();
+        } else {
+            webView.loadUrl(mainURL);
+
+
+            WebViewClient webViewClient = new WebViewClient() {
+
+
+                @Override
+                public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
+                    // DO NOT CALL SUPER METHOD
+                    super.onReceivedSslError(view, handler, error);
+                }
+
+
+
+
+
+
+                @SuppressWarnings("deprecation")
+                @Override
+
+
+
+                public boolean shouldOverrideUrlLoading(WebView view, String url) {
+
+                    switch (url) {
+                        case "url.contains(WebsiteName)":
+                            ConnectivityManager connectivityManager = (ConnectivityManager)
+                                    getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE);
+                            getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE);
+                            NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+                            if (networkInfo == null || !networkInfo.isConnected() || !networkInfo.isAvailable()) {
+                                LayoutInflater layoutInflater = getLayoutInflater();
+                                View v = layoutInflater.inflate(R.layout.oops, findViewById(R.id.oops));
+                                Toast ts = new Toast(getApplicationContext());
+                                ts.setView(v);
+                                ts.show();
+                            }
+                            break;
+                        case "url.contains(\"google.com\")":
+                            recreate();
+                            break;
+                        case "url.contains(\"youtube.com\")":
+                            Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                            startActivity(i);
+                            break;
+                        default:
+                            view.loadUrl(url);
+                    }
+
+                    return true;
+
+
+                }
+
+            };
+
+
+
+
+            webView.setWebViewClient(webViewClient);
+
+
+        }
+
+
+
+
+        class MyChrome extends WebChromeClient {
+
+
+
+
+            private View mCustomView;
+            private CustomViewCallback mCustomViewCallback;
+            protected FrameLayout mFullscreenContainer;
+            private int mOriginalOrientation;
+            private int mOriginalSystemUiVisibility;
+
+            MyChrome() {}
+
+
+
+            public Bitmap getDefaultVideoPoster()
+            {
+                if (mCustomView == null) {
+                    return null;
+                }
+                return BitmapFactory.decodeResource(getApplicationContext().getResources(), 2130837573);
+            }
+
+            public void onHideCustomView()
+            {
+                ((FrameLayout)getWindow().getDecorView()).removeView(this.mCustomView);
+                this.mCustomView = null;
+                getWindow().getDecorView().setSystemUiVisibility(this.mOriginalSystemUiVisibility);
+
+                this.mCustomViewCallback.onCustomViewHidden();
+                this.mCustomViewCallback = null;
+            }
+
+            public void onShowCustomView(View paramView, CustomViewCallback paramCustomViewCallback)
+            {
+                if (this.mCustomView != null)
+                {
+                    onHideCustomView();
+                    return;
+                }
+                this.mCustomView = paramView;
+                this.mOriginalSystemUiVisibility = getWindow().getDecorView().getSystemUiVisibility();
+                this.mOriginalOrientation = getRequestedOrientation();
+                this.mCustomViewCallback = paramCustomViewCallback;
+                ((FrameLayout)getWindow().getDecorView()).addView(this.mCustomView, new FrameLayout.LayoutParams(-1, -1));
+                getWindow().getDecorView().setSystemUiVisibility(3846);
+            }
+
+            private String TAG;
+            // For Android 3.0+
+            public void openFileChooser(ValueCallback<Uri> uploadMsg) {
+                mUploadMessage = uploadMsg;
+                Intent i = new Intent(Intent.ACTION_GET_CONTENT);
+                i.addCategory(Intent.CATEGORY_OPENABLE);
+                i.setType("image/*");
+                startActivityForResult(Intent.createChooser(i, "File Chooser"),
+                        RESULT_CODE_ICE_CREAM);
+
+            }
+            // For Android 3.0+
+            public void openFileChooser(ValueCallback uploadMsg, String acceptType) {
+                mUploadMessage = uploadMsg;
+                Intent i = new Intent(Intent.ACTION_GET_CONTENT);
+                i.addCategory(Intent.CATEGORY_OPENABLE);
+                i.setType("*/*");
+                startActivityForResult(Intent.createChooser(i, "File Browser"),
+                        RESULT_CODE_ICE_CREAM);
+            }
+            //For Android 4.1
+            public void openFileChooser(ValueCallback<Uri> uploadMsg, String acceptType, String capture) {
+                mUploadMessage = uploadMsg;
+                Intent i = new Intent(Intent.ACTION_GET_CONTENT);
+                i.addCategory(Intent.CATEGORY_OPENABLE);
+                i.setType("image/*");
+                startActivityForResult(Intent.createChooser(i, "File Chooser"),
+                        RESULT_CODE_ICE_CREAM);
+
+            }
+            //For Android5.0+
+            public boolean onShowFileChooser(
+                    WebView webView, ValueCallback<Uri[]> filePathCallback,
+                    FileChooserParams fileChooserParams) {
+                if (mFilePathCallback != null) {
+                    mFilePathCallback.onReceiveValue(null);
+                }
+                mFilePathCallback = filePathCallback;
+                Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if (takePictureIntent.resolveActivity(MainActivity.this.getPackageManager()) != null) {
+                    // Create the File where the photo should go
+                    File photoFile = null;
+                    try {
+                        photoFile = createImageFile();
+                        takePictureIntent.putExtra("PhotoPath", mCameraPhotoPath);
+                    } catch (IOException ex) {
+                        // Error occurred while creating the File
+                        Log.e(TAG, "Unable to create Image File", ex);
+                    }
+                    // Continue only if the File was successfully created
+                    if (photoFile != null) {
+                        mCameraPhotoPath = "file:" + photoFile.getAbsolutePath();
+                        takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT,
+                                Uri.fromFile(photoFile));
+                    } else {
+                        takePictureIntent = null;
+                    }
+                }
+                Intent contentSelectionIntent = new Intent(Intent.ACTION_GET_CONTENT);
+                contentSelectionIntent.addCategory(Intent.CATEGORY_OPENABLE);
+                contentSelectionIntent.setType("image/*");
+                Intent[] intentArray;
+                if (takePictureIntent != null) {
+                    intentArray = new Intent[]{takePictureIntent};
+                } else {
+                    intentArray = new Intent[0];
+                }
+                Intent chooserIntent = new Intent(Intent.ACTION_CHOOSER);
+                chooserIntent.putExtra(Intent.EXTRA_INTENT, contentSelectionIntent);
+                chooserIntent.putExtra(Intent.EXTRA_TITLE, "Image Chooser");
+                chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, intentArray);
+
+                startActivityForResult(chooserIntent, REQUEST_CODE_LOLIPOP);
+
+                return true;
+            }
+        }
+        webView.setWebChromeClient(new MyChrome());
+
+    }
+    private File createImageFile() throws IOException {
+        // Create an image file name
+
+        String imageFileName = "JPEG_";
+        File storageDir = Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES);
+        File imageFile = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+        return imageFile;
+    }
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+    private void setUpWebViewDefaults(WebView webView) {
+        WebSettings settings = webView.getSettings();
+        // Enable Javascript
+        settings.setJavaScriptEnabled(true);
+        // Use WideViewport and Zoom out if there is no viewport defined
+        settings.setUseWideViewPort(true);
+        settings.setLoadWithOverviewMode(true);
+        // Enable pinch to zoom without the zoom buttons
+        settings.setBuiltInZoomControls(true);
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.HONEYCOMB) {
+            // Hide the zoom controls for HONEYCOMB+
+            settings.setDisplayZoomControls(false);
+        }
+        // Enable remote debugging via chrome://inspect
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            WebView.setWebContentsDebuggingEnabled(true);
+        }
+        // We set the WebViewClient to ensure links are consumed by the WebView rather
+        // than passed to a browser if it can
+        webView.setWebViewClient(new WebViewClient());
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case RESULT_CODE_ICE_CREAM:
+                Uri uri = null;
+                if (data != null) {
+                    uri = data.getData();
+                }
+                mUploadMessage.onReceiveValue(uri);
+                mUploadMessage = null;
+                break;
+            case REQUEST_CODE_LOLIPOP:
+                Uri[] results = null;
+                // Check that the response is a good one
+                if (resultCode == Activity.RESULT_OK) {
+                    if (data == null) {
+                        // If there is not data, then we may have taken a photo
+                        if (mCameraPhotoPath != null) {
+                            results = new Uri[]{Uri.parse(mCameraPhotoPath)};
+                        }
+                    } else {
+                        String dataString = data.getDataString();
+                        if (dataString != null) {
+                            results = new Uri[]{Uri.parse(dataString)};
+                        }
+                    }
+                }
+                mFilePathCallback.onReceiveValue(results);
+                mFilePathCallback = null;
+                break;
+        }
+    }
+}
